c339cc816d45a4d363e8c7db0ae2cdb98ed15cf6
psoe195 <32727631+psoe195@users.noreply.github.com>
2019-01-18 17:29:02 -0800

    modify instructions for game rules

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,12 @@
 """
 Guessing game between 1 and 100
-if player's's guess is within 10, print getting close
-if player's guess is more than 10, print getting far
+if player's guess is
+    within 10 of the number, print "WARM!"
+    further than 10 away from the number, return "COLD!"
+On all subsequent turns, if a guess is
+    closer to the number than the previous guess return "WARMER!"
+    farther from the number than the previous guess, return "COLDER!"
+When the player's guess equals the number, tell them they've guessed correctly and how many guesses it took!
 """
 
 from random import randint
